c7e01b84b78c6aba2926024c4fd4d1181e9b6afe
Travis Lee <travisslee223@gmail.com>
2020-10-21 20:52:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,13 @@ require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
-
-post '/' do
-  @analyzed_text = TextAnalyzer.new(params[:user_text])
-  erb :results
-end
-
-
+  get '/' do
+    erb :index
+  end
+  
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+ 
+    erb :results
+  end
 end
--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,37 +1,43 @@
 # Your TextAnalyzer model code will go here.
 class TextAnalyzer
-  attr_accessor :text
-
+  attr_reader :text
+ 
   def initialize(text)
-    @text = text
+    @text = text.downcase
   end
-
+ 
   def count_of_words
-    @text.split(" ").count
+    words = text.split(" ")
+    words.count
   end
-
+ 
   def count_of_vowels
-    @text.downcase.scan(/[aeoui]/).count
+    text.scan(/[aeoui]/).count
   end
-
+ 
   def count_of_consonants
-    @text.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
-
-def most_used_letter
-    string = @text.downcase.gsub(/[^a-z]/, '').split('') #gets rid of spaces and turns it into an array
-    comparing_letters = string.uniq
-    most_used = ""
-    count = 0
-
-    comparing_letters.map do |letter|
-      repetitions = string.count(letter)
-      if repetitions > count
-        count = repetitions
-        most_used = letter
+ 
+  def most_used_letter
+    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+ 
+    arr1.map do |c|
+      arr2[c] =  arr.count(c)
+    end
+ 
+    biggest = { arr2.keys.first => arr2.values.first }
+ 
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
       end
     end
-    biggest = [most_used, count]
+ 
+    biggest
   end
-
 end
